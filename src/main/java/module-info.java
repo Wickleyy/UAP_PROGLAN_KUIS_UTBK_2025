069142b9104861b166f1c8ab595b59caffda5036
Wickleyy <tharifadhlurrahman@gmail.com>
2024-12-21 13:57:20 +0700

    penambahan depedensi

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
-module org.example.uap_proglan {
+module org.example.UAP_PROGLAN {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.apache.poi.ooxml;
 
     opens org.example.uap_proglan to javafx.fxml;
     exports org.example.uap_proglan;
